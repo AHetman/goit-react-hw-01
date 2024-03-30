00c8678bc3196b340ec52f366f6bd20afefbea63
AHetman <101942183+AHetman@users.noreply.github.com>
2024-03-30 23:26:19 +0200

    upd2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
-// src/components/App.jsx
-
+import Profile from "./Profile/Profile";
 import userData from "../userData.json";
-import friends from "../friends.json";
-import transactions from "../transactions.json";
-import Profile from "../components/Profile/Profile";
 import FriendList from "./FriendList/FriendList";
+import friends from "../friends.json";
 import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import transactions from "../transactions.json";
 
 const App = () => {
   return (
